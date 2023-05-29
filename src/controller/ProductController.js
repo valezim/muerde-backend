5d65d2b8c4ae9d6125449f3d66f644beb84511ba
Fatima Noble <fatinobleg@gmail.com>
2023-05-29 20:28:46 -0300

    Changed unit measure to only G, is the only unit she uses.
    Added export on controller, create of route and schema

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -61,3 +61,5 @@ class ProductController {
         }
     }
 }
+
+module.exports = ProductController;
---
+++ src/route/ProductRoute/middlewares/productValidator.js
@@ -0,0 +1,26 @@
+const {
+    postProductSchema,
+    putProductSchema,
+  } = require('../../../schema/ProductSchema');
+  
+  const validatePostProduct = (req, res, next) => {
+    const {error} = postProductSchema.validate(req.body.recipe);
+    if (error) {
+      return res.status(400).json({error: error.details[0].message});
+    }
+    next();
+  };
+  
+  const validatePutProduct = (req, res, next) => {
+    const {error} = putProductSchema.validate(req.body.recipe);
+    if (error) {
+      return res.status(400).json({error: error.details[0].message});
+    }
+    next();
+  };
+  
+  module.exports = {
+    validatePostProduct,
+    validatePutProduct,
+  };
+  
