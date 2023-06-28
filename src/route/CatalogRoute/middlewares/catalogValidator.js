7fb28a58c8440400277c3829aaea5e15527bc789
Valentina Zimberg <valentina.zimberg@mercadolibre.com>
2023-06-27 22:17:46 -0300

    fix catalog validator casesensitive import

--- src/route/CatalogRoute/middlewares/catalogValidator.js
+++ src/route/CatalogRoute/middlewares/catalogValidator.js
@@ -1,26 +1,26 @@
 const {
-    postCatalogSchema,
-    putCatalogSchema,
-  } = require('../../../schema/CatalogSchema');
-  
-  const validatePostCatalog = (req, res, next) => {
-    const {error} = postCatalogSchema.validate(req.body.catalog);
-    if (error) {
-      return res.status(400).json({error: error.details[0].message});
-    }
-    next();
-  };
-  
-  const validatePutCatalog = (req, res, next) => {
-    const {error} = putCatalogSchema.validate(req.body.catalog);
-    if (error) {
-      return res.status(400).json({error: error.details[0].message});
-    }
-    next();
-  };
-  
-  module.exports = {
-    validatePostCatalog,
-    validatePutCatalog,
-  };
-  
+  postCatalogSchema,
+  putCatalogSchema,
+} = require('../../../schema/CatalogSchema');
+
+const validatePostCatalog = (req, res, next) => {
+  const {error} = postCatalogSchema.validate(req.body.catalog);
+  if (error) {
+    return res.status(400).json({error: error.details[0].message});
+  }
+  next();
+};
+
+const validatePutCatalog = (req, res, next) => {
+  const {error} = putCatalogSchema.validate(req.body.catalog);
+  if (error) {
+    return res.status(400).json({error: error.details[0].message});
+  }
+  next();
+};
+
+module.exports = {
+  validatePostCatalog,
+  validatePutCatalog,
+};
+
