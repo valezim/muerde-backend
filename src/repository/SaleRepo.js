400b8b2dc7d6cc9efe3341581e94206f18215b6b
Valentina Zimberg <valentina.zimberg@mercadolibre.com>
2023-07-22 21:14:08 -0300

    order by

--- src/repository/SaleRepo.js
+++ src/repository/SaleRepo.js
@@ -57,7 +57,7 @@ class SaleRepo extends BaseRepo {
     }
   }
 
-  async getById({ idSale }) {
+  async getById({idSale}) {
     try {
       const sale = await this.db.Sale.findUnique({
         where: {
@@ -107,7 +107,7 @@ class SaleRepo extends BaseRepo {
     }
   }
 
-  async getSaleByUserId({ idUser }) {
+  async getSaleByUserId({idUser}) {
     try {
       const sale = await this.db.Sale.findMany({
         where: {
@@ -160,7 +160,7 @@ class SaleRepo extends BaseRepo {
   }
 
 
-  async update({ idSale, state }) {
+  async update({idSale, state}) {
     try {
       const updatedSale = await this.db.Sale.update({
         where: {
@@ -261,8 +261,8 @@ class SaleRepo extends BaseRepo {
     try {
       const statusCount = await this.db.Sale.groupBy({
         by: ['status'],
-        where: { status: { not: 'FINISHED' } },
-        _count: { status: true },
+        where: {status: {not: 'FINISHED'}},
+        _count: {status: true},
       });
       return statusCount;
     } catch (error) {
@@ -293,7 +293,7 @@ class SaleRepo extends BaseRepo {
             },
           },
         ],
-        _count: { idSale: true },
+        _count: {idSale: true},
       });
 
       return totalSalesByCustomer;
@@ -319,6 +319,9 @@ class SaleRepo extends BaseRepo {
           start_date: {
             lte: formattedEndDate,
           },
+        },
+        orderBy: {
+          finish_date: 'asc',
         },
         select: {
           idSale: true,
@@ -361,6 +364,9 @@ class SaleRepo extends BaseRepo {
           finish_date: {
             not: null,
           },
+        },
+        orderBy: {
+          finish_date: 'asc',
         },
         select: {
           idSale: true,
