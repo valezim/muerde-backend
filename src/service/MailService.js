0172d35c63d8fe554c6c7af86e51c7a199c0ec1f
Fatima Noble <fatinobleg@gmail.com>
2023-08-26 22:01:27 -0300

    catch email sending error

--- src/service/MailService.js
+++ src/service/MailService.js
@@ -22,28 +22,40 @@ class MailService {
     }
 
     async sendPurchaseConfirmation(email) {
-        let mailOptions = {
-            from: 'Muerde Repostería <muerde.reposteria@outlook.com>',
-            to: email,
-            subject: "Confirmación de compra",
-            text: "Gracias por tu compra",
-            html: purchaseConfirmationHTML,
-        };
-
-        return await this.transporter.sendMail(mailOptions);
+        try {
+            let mailOptions = {
+                from: 'Muerde Repostería <muerde.reposteria@outlook.com>',
+                to: email,
+                subject: "Confirmación de compra",
+                text: "Gracias por tu compra",
+                html: purchaseConfirmationHTML,
+            };
+    
+            const result = await this.transporter.sendMail(mailOptions);
+            return { success: true, result };
+        } catch (error) {
+            console.error("Error enviando correo de confirmación de compra:", error);
+            return { success: false, error };
+        }
     }
-
+    
     async sendReviewRequest(email) {
-        let mailOptions = {
-            from: 'Muerde Repostería <muerde.reposteria@outlook.com>',
-            to: email,
-            subject: "Solicitud de reseña",
-            text: "Por favor, deja una reseña para tu reciente compra",
-            html: reviewRequestHTML,
-        };
-
-        return await this.transporter.sendMail(mailOptions);
-    }
+        try {
+            let mailOptions = {
+                from: 'Muerde Repostería <muerde.reposteria@outlook.com>',
+                to: email,
+                subject: "Solicitud de reseña",
+                text: "Por favor, deja una reseña para tu reciente compra",
+                html: reviewRequestHTML,
+            };
+    
+            const result = await this.transporter.sendMail(mailOptions);
+            return { success: true, result };
+        } catch (error) {
+            console.error("Error enviando correo de solicitud de reseña:", error);
+            return { success: false, error };
+        }
+    }    
 }
 
 module.exports = new MailService();
